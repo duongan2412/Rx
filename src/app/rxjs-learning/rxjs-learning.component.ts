e1669f2a02c2ee3c2c2566341713f33a73f31544
duongan2412 <76814577+duongan2412@users.noreply.github.com>
2023-04-17 12:50:40 +0700

    1 file modified, 1 added

--- src/app/rxjs-learning/rxjs-learning.component.ts
+++ src/app/rxjs-learning/rxjs-learning.component.ts
@@ -1,36 +1,65 @@
 import { Component, OnInit } from '@angular/core';
+import moment = require('moment');
 import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-rxjs-learning',
   templateUrl: './rxjs-learning.component.html',
-  styleUrls: ['./rxjs-learning.component.scss']
+  styleUrls: ['./rxjs-learning.component.scss'],
 })
 export class RxjsLearningComponent implements OnInit {
-
   agent!: Observable<string>;
   agentName!: string;
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
-    this.agent = new Observable(
-      function(observer) {
-        try{
-          observer.next('Hello');
-          setInterval(() => {
-            observer.next('Hi');
-          }, 4000)
-          setInterval(() => {
-            observer.next('Good morning');
-          }, 8000)
-        }
-        catch(e) {
-          observer.error(`Oops, something went wrong ${e}`);
-        }
-      }
-    )
-
-    this.agent.subscribe((data) => this.agentName = data);
+    // this.agent = new Observable(
+    //   function(observer) {
+    //     try{
+    //       observer.next('Hello');
+    //       setInterval(() => {
+    //         observer.next('Hi');
+    //       }, 4000)
+    //       setInterval(() => {
+    //         observer.next('Good morning');
+    //       }, 8000)
+    //     }
+    //     catch(e) {
+    //       observer.error(`Oops, something went wrong ${e}`);
+    //     }
+    //   }
+    // )
+    // this.agent.subscribe((data) => this.agentName = data);
+    const amoount = this.calRoomAmount(
+      1500,
+      0,
+      moment('2023-04-18', 'YYYY-MM-DD'),
+      moment('2023-03-18', 'YYYY-MM-DD')
+    );
+    console.log('amoount', amoount);
+    console.log('moment', moment().startOf('day'));
   }
 
+  calRoomAmount(
+    monthPrice: number,
+    discount: number,
+    periodDate: moment.Moment,
+    termPaidDate: moment.Moment
+  ) {
+    periodDate = periodDate.startOf('days');
+    termPaidDate = termPaidDate.startOf('days');
+    console.log('calRoomAmount', {
+      periodDate,
+      termPaidDate,
+      momentDays: moment().diff(periodDate, 'days'),
+      month: Math.ceil(moment().startOf('day').diff(periodDate, 'months')),
+    });
+    if (moment().diff(periodDate, 'days') >= 0) {
+      let months = Math.ceil(moment().diff(periodDate, 'months'));
+      console.log('months', months);
+      months = months > 1 ? months : 1;
+      return (monthPrice - discount) * months;
+    }
+    return 0;
+  }
 }
